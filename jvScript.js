4d17cee1a505567e26e6b0d4957c9af2bc0e7cd8
tmilitino <tmilitino.tm@gmail.com>
2018-02-28 09:57:56 -0300

    regras de validação - RB
    regras de validação para receita bruta

--- jvScript.js
+++ jvScript.js
@@ -12,3 +12,29 @@ var cChecked = function () {
         };
         cChecked();
         $("input[type=checkbox]").on("click", cChecked);
+        
+        $("#res").hide();
+        $("#com").hide();
+
+        $(document).ready(function () {
+            //called when key is pressed in textbox
+            $('#inlineFormInputGroupReceitaBruta').keypress(function (e) {
+               //if the letter is not digit then display error and don't type anything
+               if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                  //display error message
+                  $("#errmsg").html("Só numeros").show().fadeOut("slow");
+                         return false;
+              }
+             });
+          });
+
+
+      $('#inlineFormInputGroupReceitaBruta').change(function(){
+            if($('#inlineFormInputGroupReceitaBruta').val()>36000000){
+                $("#res").hide();
+                $("#com").show();
+            }else{
+                $("#com").hide();
+                $("#res").show();
+                }
+            });
