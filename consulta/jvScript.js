d34c121e4ccb63379ab0c287ba0357319e4867fd
tmilitino <tmilitino.tm@gmail.com>
2018-03-01 14:35:54 -0300

    n

--- consulta/jvScript.js
+++ consulta/jvScript.js
@@ -88,3 +88,32 @@ $(document).ready(function () {
         thousands: "."
     });
 });
+
+/*$(document).ready(function(){
+    
+    load_data();
+   
+    function load_data(query)
+    {
+     $.ajax({
+      url:"consulta.php",
+      method:"POST",
+      data:{query:query},
+      success:function(data)
+      {
+       $('#result').html(data);
+      }
+     });
+    }
+    $('#search_text').keyup(function(){
+     var search = $('#search_text').val();
+     if(search != '')
+     {
+      load_data(search);
+     }
+     else
+     {
+      load_data();
+     }
+    });
+   });*/
